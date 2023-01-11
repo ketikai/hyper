d3cd67606b84b7cfab97217cf3ef9e5f94334e6f
ketikai <ketikai@qq.com>
2023-01-11 22:50:43 +0800

    style(readme.md): 修改显示

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,22 +5,6 @@ buildscript {
             name = "aliyun-public"
             url = uri("https://maven.aliyun.com/repository/public")
         }
-        maven {
-            name = "ketikai-aliyun-release"
-            url = uri("https://packages.aliyun.com/maven/repository/2316884-release-1uE4Wz")
-            credentials {
-                username = "63a6e5de3615b942064ae00a"
-                password = "9]NhW119yofE"
-            }
-        }
-        maven {
-            name = "ketikai-aliyun-snapshot"
-            url = uri("https://packages.aliyun.com/maven/repository/2316884-snapshot-OMR1CL")
-            credentials {
-                username = "63a6e5de3615b942064ae00a"
-                password = "9]NhW119yofE"
-            }
-        }
         mavenCentral()
     }
 
@@ -50,22 +34,6 @@ subprojects {
         maven {
             name = "aliyun-public"
             url = uri("https://maven.aliyun.com/repository/public")
-        }
-        maven {
-            name = "ketikai-aliyun-release"
-            url = uri("https://packages.aliyun.com/maven/repository/2316884-release-1uE4Wz")
-            credentials {
-                username = "63a6e5de3615b942064ae00a"
-                password = "9]NhW119yofE"
-            }
-        }
-        maven {
-            name = "ketikai-aliyun-snapshot"
-            url = uri("https://packages.aliyun.com/maven/repository/2316884-snapshot-OMR1CL")
-            credentials {
-                username = "63a6e5de3615b942064ae00a"
-                password = "9]NhW119yofE"
-            }
         }
         mavenCentral()
     }
